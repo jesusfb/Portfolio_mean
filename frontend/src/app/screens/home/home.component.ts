7b64777412467e4db6195bd1b8e291dbceeeb122
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-07-19 16:34:57 -0600

    Estilizado de Acerca de mi, Habilidades, y componente landing-tech-card

--- frontend/src/app/screens/home/home.component.ts
+++ frontend/src/app/screens/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LandingTechCardComponent } from "../../components/landing-tech-card/landing-tech-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [LandingTechCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
