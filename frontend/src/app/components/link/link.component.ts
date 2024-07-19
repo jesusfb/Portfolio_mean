dde83096e956c58babff9de6d97d69f3571c3045
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-07-19 13:00:52 -0600

    Estilizado básico de navbar y sección hero, componente link, falta agregar animaciones y optimizar espacio en tamaños sm .. md

---
+++ frontend/src/app/components/link/link.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './link.component.html',
+  styleUrl: './link.component.css'
+})
+export class LinkComponent {
+  @Input() href: string = '#';
+  @Input() text: string = 'Link';
+}
