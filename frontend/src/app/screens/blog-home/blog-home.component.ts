5f10ddaf8f0b34e0f976bb1839e1b3c2dd688840
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-07-29 14:03:54 -0600

    Estilizado responsivo de pantalla de inicio de blog/

--- frontend/src/app/screens/blog-home/blog-home.component.ts
+++ frontend/src/app/screens/blog-home/blog-home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LinkComponent } from "../../components/link/link.component";
 
 @Component({
   selector: 'app-blog-home',
   standalone: true,
-  imports: [],
+  imports: [LinkComponent],
   templateUrl: './blog-home.component.html',
   styleUrl: './blog-home.component.css'
 })
