5f98353be914634e00fceb1204896da1b37f077c
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-07-31 09:30:26 -0600

    Estilizando pantalla de perfil, algunos estilos comunes, y componente estático de blog-post-card

--- frontend/src/app/screens/blog-profile/blog-profile.component.ts
+++ frontend/src/app/screens/blog-profile/blog-profile.component.ts
@@ -6,7 +6,7 @@ import { LinkComponent } from '../../components/link/link.component';
 @Component({
   selector: 'app-blog-profile',
   standalone: true,
-  imports: [SvgIconComponent, LinkComponent],
+  imports: [SvgIconComponent, LinkComponent, BlogPostCardComponent],
   templateUrl: './blog-profile.component.html',
   styleUrl: './blog-profile.component.css'
 })
